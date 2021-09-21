606a47f572889ff25724fc1ebb825cd78e2144f5
arshad-qadri <arshadqadri321@gmail.com>
2021-09-21 12:15:01 +0530

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import data from './data';
+
+const btn = [...new Set(data.map((elem) => elem.category)),"all"];
+console.log( 'btn', btn);
 
 function App() {
+  const [item, setItem]= useState(data)
+  console.log(data);
+
+const btnHandle=(item)=>{
+  const updatedData= data.filter(e=>e.category===item)
+  console.log(item);
+  setItem(updatedData)
+  if(item==="all"){
+      setItem(data);
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1
+        className="bg-red-500 h-16 text-center text-white font-bold text-3xl tracking-wider capitalize"
+        style={{ lineHeight: "4rem" }}
+      >
+        Your favourite dish
+      </h1>
+
+      <div className="flex justify-center items-center gap-x-6 my-9 ">
+        {btn.map((item,i) => {
+          return (
+            <button key={i} onClick={()=>btnHandle(item)} className="bg-yellow-400 px-6 py-1 text-white capitalize font-bold tracking-wider shadow-xl rounded hover:bg-yellow-500">
+              {item}
+            </button>
+          );
+        })}
+      </div>
+      <div className="w-5/6 mx-auto  mt-4 flex flex-wrap gap-4">
+        {item.map((elem) => {
+          return (
+            <div
+              className="bg-white shadow-2xl w-96 h-52 grid grid-cols-3 rounded-xl overflow-hidden"
+              key={elem.id}
+            >
+              <div className="bg-yellow-200 overflow-hidden">
+                <img
+                  src={elem.img}
+                  alt={elem.name}
+                  className=" h-full object-cover transform hover:scale-150 duration-1000"
+                />
+              </div>
+              <div className="border col-span-2 p-3">
+                <h1 className="text-2xl font-bold"> {elem.name} </h1>
+                <p className="text-sm font-semibold my-4">{elem.desc}</p>
+                <div className="flex justify-between items-center mt-8  ">
+                  <h2 className="text-yellow-400 font-bold text-lg">
+                    {`Price : ${elem.price} ₹`}
+                  </h2>
+                  <button className="bg-yellow-400 text-white font-bold text-sm px-3 py-1 shadow-lg rounded">
+                    Order Now
+                  </button>
+                </div>
+                <p className="text-sm font-bold mt-4 text-gray-400">
+                  *Prices may vary on selected date
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+//  <div className="container mx-auto mt-4 ">
+//    <div className="bg-white shadow-2xl w-96 h-52 grid grid-cols-3 rounded-xl overflow-hidden">
+//      <div className="bg-yellow-200 ">
+//        <img
+//          src="https://vismaifood.com/storage/app/uploads/public/e12/7b7/127/thumb__1200_0_0_0_auto.jpg"
+//          alt=""
+//          className=" h-full object-cover"
+//        />
+//      </div>
+//      <div className="border col-span-2 p-3">
+//        <h1 className="text-2xl font-bold">Name</h1>
+//        <p className="text-sm font-semibold my-4">
+//          Lorem Ipsum is simply dummy text of typesetting industry.
+//        </p>
+//        <div className="flex justify-between items-center mt-8  ">
+//          <h2 className="text-yellow-400 font-bold text-lg">Price : 30 ₹</h2>
+//          <button className="bg-yellow-400 text-white font-bold text-sm px-3 py-1 shadow-lg rounded">
+//            Order Now
+//          </button>
+//        </div>
+//        <p className="text-sm font-bold mt-4 text-gray-400">
+//          *Prices may vary on selected date
+//        </p>
+//      </div>
+//    </div>
+//  </div>;
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data = [
+  {
+    id: 1,
+    img: "https://lovelaughmirch.com/wp-content/uploads/2013/08/Aloo-ka-Paratha-_10.jpg",
+    name: "Aloo Paratha",
+    category: "breakfast",
+    price: "30",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  {
+    id: 2,
+    img: "https://static.toiimg.com/thumb/61050397.cms?width=1200&height=900",
+    name: "Samosa",
+    category: "evening",
+    price: "10",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  {
+    id: 3,
+    img: "https://vismaifood.com/storage/app/uploads/public/e12/7b7/127/thumb__1200_0_0_0_auto.jpg",
+    name: "Biryani",
+    category: "dinner",
+    price: "80",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  {
+    id: 4,
+    img: "https://www.whiskaffair.com/wp-content/uploads/2020/07/Onion-Pakoda-3.jpg",
+    name: "Pakoda",
+    category: "breakfast",
+    price: "30",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  {
+    id: 5,
+    img: "https://drizzlingdelicacies.files.wordpress.com/2020/07/corn-on-the-cob-monsoon-1.jpg?w=1568",
+    name: "Corn",
+    category: "evening",
+    price: "20",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  {
+    id: 6,
+    img: "https://imgk.timesnownews.com/story/1544072073-rice.PNG?tr=w-560,h-292,fo-top",
+    name: "Rice",
+    category: "dinner",
+    price: "50",
+    desc: "Lorem Ipsum is simply dummy text of typesetting industry.",
+  },
+  
+];
+export default data
